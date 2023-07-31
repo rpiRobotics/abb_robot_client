5c4e239654177c7a4f2d5f2f285b4acc2647585e
John Wason <wason@wasontech.com>
2023-07-30 23:12:46 -0400

    Add support for new fields in egm protocol

--- src/abb_robot_client/egm.py
+++ src/abb_robot_client/egm.py
@@ -33,8 +33,21 @@ class EGMRobotState(NamedTuple):
     robot_message: Any
     """Full message returned from robot"""
     cartesian: Tuple[np.array,np.array]
-    """Cartesian position of robots, in ([x,y,z],[w,x,y,z]) format"""
-
+    """Cartesian position of robot, in ([x,y,z],[w,x,y,z]) format"""
+    joint_angles_planned: np.array
+    """Planned (commanded) joint angles of robot in degrees. Length is 6 or 7 depending on robot"""
+    cartesian_planned: Tuple[np.array,np.array]
+    """Planned (commanded) cartesian position of robot, in ([x,y,z],[w,x,y,z]) format"""
+    external_axes: np.array
+    """External axes in degrees"""
+    external_axes_planned: np.array
+    """Planned (commanded) external axes in degrees"""
+    measured_force: np.array
+    """Measured force wrench in Nm and N"""
+    move_index: int
+    """Move index of current motion"""
+    rapid_from_robot: np.array
+    """RAPID variables from robot"""
 
 
 class EGM(object):
@@ -90,6 +103,13 @@ class EGM(object):
         rapid_running=False
         motors_on=False
         cartesian=None
+        external_axes= None
+        joint_angles_planned = None
+        cartesian_planned = None
+        external_axes_planned = None
+        measured_force = None
+        move_index = None
+        rapid_from_robot = None
 
         if robot_message.HasField('feedBack'):
             joints=robot_message.feedBack.joints.joints
@@ -98,16 +118,52 @@ class EGM(object):
                 cart_p = robot_message.feedBack.cartesian.pos
                 cart_q = robot_message.feedBack.cartesian.orient
                 cartesian = (np.array([cart_p.x,cart_p.y,cart_p.z]),np.array([cart_q.u0,cart_q.u1,cart_q.u2,cart_q.u3]))
+            if robot_message.feedBack.HasField('externalJoints'):
+                external_axes = np.array(list(robot_message.feedBack.externalJoints.joints))
         if robot_message.HasField('rapidExecState'):
             rapid_running = robot_message.rapidExecState.state == robot_message.rapidExecState.RAPID_RUNNING
         if robot_message.HasField('motorState'):
             motors_on = robot_message.motorState.state == robot_message.motorState.MOTORS_ON
-        
-            
+        if robot_message.HasField('planned'):
+            planned = robot_message.planned
+            if planned.HasField('joints'):
+                joints = planned.joints.joints
+                joint_angles_planned = np.array(list(joints))
+            if planned.HasField('cartesian'):
+                cart_p = planned.cartesian.pos
+                cart_q = planned.cartesian.orient
+                cartesian_planned = (np.array([cart_p.x,cart_p.y,cart_p.z]),np.array([cart_q.u0,cart_q.u1,cart_q.u2,cart_q.u3]))
+            if planned.HasField('externalJoints'):
+                external_axes_planned = np.array(list(planned.externalJoints.joints))
+        if robot_message.HasField('measuredForce'):
+            force_active = True
+            if robot_message.measuredForce.HasField('fcActive'):
+                force_active = robot_message.measuredForce.fcActive
+            if force_active:
+                force = robot_message.measuredForce.force
+                measured_force = np.array(list(force))
+        if robot_message.HasField('RAPIDfromRobot'):
+            rapid_from_robot = np.array(list(robot_message.RAPIDfromRobot.dnum))
+        if robot_message.HasField('moveIndex'):
+            move_index = robot_message.moveIndex
 
-        return True, EGMRobotState(joint_angles, rapid_running, motors_on, robot_message, cartesian)
+        return True, EGMRobotState(
+            joint_angles=joint_angles,
+            rapid_running=rapid_running,
+            motors_on=motors_on,
+            robot_message=robot_message,
+            cartesian=cartesian,
+            joint_angles_planned=joint_angles_planned,
+            cartesian_planned=cartesian_planned,
+            external_axes=external_axes,
+            external_axes_planned=external_axes_planned,
+            measured_force=measured_force,
+            move_index=move_index,
+            rapid_from_robot=rapid_from_robot
+        )
 
-    def send_to_robot(self, joint_angles: np.array) -> bool:
+    def send_to_robot(self, joint_angles: np.array, speed_ref: np.array = None, external_joints: np.array = None,
+                      external_joints_speed: np.array = None, rapid_to_robot: np.array = None) -> bool:
         """
         Send a joint command to robot. Returns False if no data has been received from the robot yet. The EGM
         operation must have been started with EGMActJoint and EGMRunJoint.
@@ -129,10 +185,27 @@ class EGM(object):
         self.send_sequence_number+=1
 
         planned=sensorMessage.planned
+        speedRef = sensorMessage.speedRef
 
         if joint_angles is not None:
             joint_angles2 = list(np.array(joint_angles))
             planned.joints.joints.extend(joint_angles2)
+
+        if speed_ref is not None:
+            speed_ref2 = list(np.array(speed_ref))
+            speedRef.joints.joints.extend(speed_ref2)
+
+        if external_joints is not None:
+            external_joints2 = list(np.array(external_joints))
+            planned.externalJoints.joints.extend(external_joints2)
+
+        if external_joints_speed is not None:
+            external_joints_speed2 = list(np.array(external_joints_speed))
+            speedRef.externalJoints.joints.extend(external_joints_speed2)
+
+        if rapid_to_robot is not None:
+            rapid_to_robot2 = list(np.array(rapid_to_robot))
+            sensorMessage.RAPIDtoRobot.dnum.extend(rapid_to_robot2)
 
         buf2=sensorMessage.SerializeToString()
 
@@ -143,7 +216,9 @@ class EGM(object):
 
         return True
 
-    def send_to_robot_cart(self, pos: np.ndarray, orient: np.ndarray):
+    def send_to_robot_cart(self, pos: np.ndarray, orient: np.ndarray, speed_ref: np.array = None, 
+                           external_joints: np.array = None, external_joints_speed: np.array = None, 
+                           rapid_to_robot: np.array = None):
         """
         Send a cartesian command to robot. Returns False if no data has been received from the robot yet. The pose
         is relative to the tool, workobject, and frame specified when the EGM operation is initialized. The EGM
@@ -166,6 +241,7 @@ class EGM(object):
         self.send_sequence_number+=1
 
         planned=sensorMessage.planned
+        speedRef = sensorMessage.speedRef
 
         # if joint_angles is not None:
         #     joint_angles2 = list(np.rad2deg(joint_angles))
@@ -179,6 +255,22 @@ class EGM(object):
             planned.cartesian.orient.u1 = orient[1]
             planned.cartesian.orient.u2 = orient[2]
             planned.cartesian.orient.u3 = orient[3]
+
+        if speed_ref is not None:
+            speed_ref2 = list(np.array(speed_ref))
+            speedRef.cartesians.value.extend(speed_ref2)
+
+        if external_joints is not None:
+            external_joints2 = list(np.array(external_joints))
+            planned.externalJoints.joints.extend(external_joints2)
+
+        if external_joints_speed is not None:
+            external_joints_speed2 = list(np.array(external_joints_speed))
+            speedRef.externalJoints.joints.extend(external_joints_speed2)
+
+        if rapid_to_robot is not None:
+            rapid_to_robot2 = list(np.array(rapid_to_robot))
+            sensorMessage.RAPIDtoRobot.dnum.extend(rapid_to_robot2)
 
         buf=sensorMessage.SerializeToString()
 
